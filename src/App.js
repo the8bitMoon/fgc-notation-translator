3a016f3c1257b609a5a21b450c7b0db9e0e165a7
the8bitMoon <27790800+the8bitMoon@users.noreply.github.com>
2019-11-04 21:01:58 -0600

    Added component files.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Body from './components/body.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      page: 'main',
+      map: '',
+      combo: [],
+      notation: '5A > 5B > 5C > 236A',
+      textOutput: ''
+    }
+  }
+
+  render() {
+    return(
+      <div className="App">
+        App will go here.
+        <Body
+          map={this.state.map}
+          combo={this.state.combo}
+          notation={this.state.notation}
+          textOuput={this.state.textOutput}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/body.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Body(props) {
+  return(
+    <div className="Body">
+      App body.
+      <p>{props.notation}</p>
+    </div>
+  )
+}
